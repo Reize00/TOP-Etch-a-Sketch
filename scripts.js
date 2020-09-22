c2d859466d252076235583eb330c3b80ed6abb57
Wesslee Whatley <wesslee.whatley@gmail.com>
2020-09-22 14:10:43 -0500

    Create squares that change color on mouseover, add button to clear grid

--- scripts.js
+++ scripts.js
@@ -1,28 +1,42 @@
-function initDynamicGrid(size) {
-    const totalSquares = size * size;
-    let rowsString = "";
-    let columnsString = "";
-    for(i = 0; i < size; i++) {
-        if(i === size -1) {
-            rowsString +='1fr';
-        } else {
-            rowsString +='1fr ';
+function resetGrid() {
+    // delete container full of squares, clearing the page
+    let target = document.querySelector('.container');
+    target.remove();
+
+    let newSize = NaN;
+    // prompt for a new container size and create a new grid
+    do{
+        newSize = prompt('Please enter a size for the new grid.');
+        if(isNaN(newSize) || newSize <= 0) {
+            alert("That isn't a valid number!");
         }
-        if(i === size -1) {
-            columnsString += '1fr';
-        } else {
-            columnsString += '1fr ';
-        }
-    }
-
-    let gridContainer = document.createElement('div');
-    gridContainer.style.display = "grid";
-    gridContainer.style.gridTemplateRows = rowsString;
-    gridContainer.style.gridTemplateColumns = columnsString;
-
-    for(i = 0; i < totalSquares; i++) {
-        let squareInGrid = document.createElement('div');
-
-        gridContainer.appendChild(squareInGrid);
-    }
+    }while(isNaN(newSize) || newSize <= 0);
+    createGrid(newSize);
 }
+
+function changeColor(e) {
+    e.target.style.backgroundColor = "#182628";
+}
+
+function createGrid(size) {
+    const totalSquares = size * size;
+    // create a contianer for the squares
+    let gridContainer = document.createElement('div');
+    gridContainer.setAttribute("class", "container");
+    gridContainer.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+    // fill the container with squares
+    for(i = 0; i < totalSquares; i++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.setAttribute("class", "gridSquare");
+        gridSquare.setAttribute("id", i);
+        gridContainer.appendChild(gridSquare);    
+    }
+    document.body.appendChild(gridContainer);
+    // add mouseover listeners to all the squares
+    const squares = document.querySelectorAll('.gridSquare');
+    squares.forEach(square => square.addEventListener('mouseover', changeColor));
+}
+
+document.getElementById("reset").addEventListener("click", resetGrid);
+
+createGrid(16);
